2e3776cd281b0295b3ae25a2165a8974929cb699
Lexsak <bartoszpiprek123@gmail.com>
2024-01-10 20:45:02 +0100

    linear gradient in hero-title

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColorService } from 'src/app/color.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  gradient: string = 'linear-gradient(to right, red, green)';
+  // Colors from Toolbar
+  constructor(private colorService: ColorService) {}
+
+  getTextColor() {
+    return this.colorService.textColor;
+  }
+
+  getBackgroundColor() {
+    return this.colorService.backgroundColor;
+  }
+
+  getPrimaryColor() {
+    return this.colorService.primaryColor;
+  }
+
+  getSecondaryColor() {
+    return this.colorService.secondaryColor;
+  }
+
+  getAccentColor() {
+    return this.colorService.accentColor;
+  }
+
+  getGradient() {
+    return {
+      background: `linear-gradient(to right, ${this.getPrimaryColor()} 10%, ${this.getAccentColor()})  text`,
+      color: 'transparent',
+    };
+  }
 }
