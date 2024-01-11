c3bf956de6c9d8e2cf94c796502e3c6c24acd523
Lexsak <bartoszpiprek123@gmail.com>
2024-01-11 16:36:45 +0100

    Simple Blocks & Tiles html

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -37,9 +37,40 @@ export class HomeComponent {
     };
   }
 
-  getTextStroke(){
+  getTextStroke() {
     return {
       '-webkit-text-stroke': `1.5px ${this.getTextColor()}`,
     };
+  }
+
+  getRGBA(color: string) {
+    
+
+    const rgbaColor = this.hexOrRgbToRgba(color, 0.2);
+
+    return rgbaColor;
+  }
+
+  hexOrRgbToRgba(color: string, alpha: number): string {
+    // Sprawdź, czy kolor jest w formie HEX
+    if (color[0] === '#') {
+      // Konwersja koloru HEX na RGBA
+      const hexColor = color.substring(1); // Usuń znak '#' z początku
+      const bigint = parseInt(hexColor, 16);
+      const r = (bigint >> 16) & 255;
+      const g = (bigint >> 8) & 255;
+      const b = bigint & 255;
+
+      return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+    } else if (color.startsWith('rgb')) {
+      // Konwersja koloru RGB na RGBA
+      const rgbValues = color.match(/\d+/g)?.map(Number); // Wyciągnij wartości liczbowe
+      if (rgbValues && rgbValues.length === 3) {
+        return `rgba(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]}, ${alpha})`;
+      }
+    }
+
+    // Jeśli kolor nie jest ani w formie HEX, ani RGB, zwróć oryginalny kolor
+    return color;
   }
 }
