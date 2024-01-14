702d552a995cbdf6cf226637897b9e35149ce3d6
Lexsak <bartoszpiprek123@gmail.com>
2024-01-14 20:40:25 +0100

    better appearance of the dropdown-menu

--- src/app/layouts/color-toolbar/color-toolbar.component.ts
+++ src/app/layouts/color-toolbar/color-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ColorService } from 'src/app/color.service';
 
 interface ColorPalettes {
@@ -12,7 +12,7 @@ interface ColorPalettes {
   templateUrl: './color-toolbar.component.html',
   styleUrls: ['./color-toolbar.component.css'],
 })
-export class ColorToolbarComponent {
+export class ColorToolbarComponent implements OnInit {
   color: string = '#808080';
 
   // Colors from Toolbar
@@ -68,32 +68,39 @@ export class ColorToolbarComponent {
   colorPalettes: ColorPalettes[] = [
     {
       id: 1,
-      name: 'Blue',
-      colors: ['#e7f3f1', '#091715', '#98e5db', '#189a8a', '#18efd4'],
+      name: 'Default',
+      colors: ['#06150c', '#f7fdfa', '#6d97d5', '#92cddf', '#3cc77b'],
     },
     {
       id: 2,
-      name: 'Yellow Dark',
-      colors: ['#0b0a06', '#f9f8f4', '#a79d5b', '#9dcabc', '#7da6b9'],
+      name: 'Blue',
+      colors: ['#e7f3f1', '#091715', '#98e5db', '#189a8a', '#18efd4'],
     },
+
     {
       id: 3,
-      name: 'Pink',
-      colors: ['#fce3f7', '#1d0318', '#f383d1', '#930f10', '#ea6c2d'],
-    },
-    {
-      id: 4,
       name: 'Purple',
       colors: ['#eae7f7', '#07050f', '#a69ede', '#642876', '#c553c5'],
     },
     {
-      id: 5,
+      id: 4,
       name: 'Green',
       colors: ['#f0f5e8', '#141a0b', '#c1d8a4', '#3e7532', '#56b554'],
     },
+    {
+      id: 5,
+      name: 'Dark Yellow ',
+      colors: ['#0b0a06', '#f9f8f4', '#a79d5b', '#9dcabc', '#7da6b9'],
+    },
+    {
+      id: 6,
+      name: 'Dark Pink',
+      colors: ['#fce3f7', '#1d0318', '#f383d1', '#930f10', '#ea6c2d'],
+    },
   ];
 
-  isDropDown: boolean = false;
+  isDropDown: boolean = true;
+  activeColorPalette: ColorPalettes | null = null;
 
   @HostListener('document:click', ['$event'])
   handleDocumentClick(event: Event) {
@@ -111,13 +118,21 @@ export class ColorToolbarComponent {
     this.isDropDown = !this.isDropDown;
   }
 
+  ngOnInit() {
+    // Ustaw domyślną opcję na "Default" przy inicjalizacji komponentu
+    this.activeColorPalette =
+      this.colorPalettes.find((palette) => palette.name === 'Default') ?? null;
+  }
+
   changeColor(
     hax1: string,
     hax2: string,
     hax3: string,
     hax4: string,
-    hax5: string
+    hax5: string,
+    colorPalette: ColorPalettes
   ) {
+    this.activeColorPalette = colorPalette;
     this.colorService.setTextColor(hax1);
     this.colorService.setBackgroundColor(hax2);
     this.colorService.setPrimaryColor(hax3);
