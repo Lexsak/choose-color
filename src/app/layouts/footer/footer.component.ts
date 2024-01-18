46ba13956aec47070128557b07935a1f26a03b83
Lexsak <bartoszpiprek123@gmail.com>
2024-01-18 21:55:47 +0100

    footer Social section html, css, ts

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ColorService } from 'src/app/color.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  // Colors from Toolbar
+  constructor(private colorService: ColorService) {}
 
+  getBackgroundColor() {
+    return this.colorService.backgroundColor;
+  }
+
+  getTextColor() {
+    return this.colorService.textColor;
+  }
 }
