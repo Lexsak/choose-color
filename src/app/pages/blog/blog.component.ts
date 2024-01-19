883a05b287a3349e57fdd9e4bd980daf484ee7a4
Lexsak <bartoszpiprek123@gmail.com>
2024-01-19 23:41:00 +0100

    Blog first setup

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ColorService } from 'src/app/color.service';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent {
+  constructor(private colorsService: ColorService) {}
 
+  getTextColor() {
+    return this.colorsService.textColor;
+  }
 }
